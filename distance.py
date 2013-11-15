1e25eacc48acaf93386acbb55b86812cbed19a3d
mcashore <massey.cashore@gmail.com>
2013-11-15 15:15:07 -0800

    song files

--- distance.py
+++ distance.py
@@ -12,20 +12,19 @@ weight_params = {
 }
 
 def euclidean_distance(v1, v2):
-    sq_diff = [(a - b) ** 2 for a,b in zip(v1, v2)]
-    return sqrt(sum(sq_diff))
+    sum_sq_diff = 0
+    for i in range(len(v1)):
+        sum_sq_diff += (v1[i] - v2[i]) ** 2
+    return sqrt(sum_sq_diff)
 
 def distance_beats(beat1, beat2):
     """ distance between two AudioQuantum beatments """
-    distances = {
-        'pitch' : euclidean_distance(beat1.mean_pitches(), beat2.mean_pitches()),
-        'timbre' : euclidean_distance(beat1.mean_timbre(), beat2.mean_timbre()),
-        'mean_loudness' : abs(beat1.mean_loudness() - beat2.mean_loudness()),
-        'confidence' : abs(beat1.confidence - beat2.confidence),
-        'duration' : abs(beat1.duration - beat2.duration),
-    }
-    distance = 0
-    for kind, distance in distances.iteritems():
-        distance += distance * weight_params[kind]
-    return distance
+    pitch = euclidean_distance(beat1.mean_pitches(), beat2.mean_pitches())
+    timbre = euclidean_distance(beat1.mean_timbre(), beat2.mean_timbre())
+    mean_loudness = abs(beat1.mean_loudness() - beat2.mean_loudness())
+    confidence = abs(beat1.confidence - beat2.confidence)
+    duration = abs(beat1.duration - beat2.duration)
+
+    return weight_params['pitch'] * pitch + weight_params['timbre'] * timbre + weight_params['mean_loudness'] * mean_loudness + \
+            weight_params['confidence'] * confidence + weight_params['duration'] * duration
 
